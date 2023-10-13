9e27584f83f10282e90376223c5f0930adbd2697
okpocandy <opcandy@naver.com>
2023-10-13 16:40:03 +0900

    add C# code Study

---
+++ CodeS/cS13_6.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeS
+{
+    delegate void EventHandler(string message);
+
+    class MyNotifier
+    {
+        public event EventHandler SomethingHappend;
+        public void DoSomething(int number)
+        {
+            int temp = number % 10;
+
+            if(temp != 0 && temp % 3 == 0)
+            {
+                SomethingHappend(String.Format("{0} : 짝", number));
+            }
+        }
+    }
+    internal class cS13_6
+    {
+        static public void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static public void MyHandler2(string message)
+        {
+            Console.WriteLine($" Handler 2 : {message}");
+        }
+        static void Main(string[] args)
+        {
+            MyNotifier notifier = new MyNotifier();
+            notifier.SomethingHappend += new EventHandler(MyHandler);
+            notifier.SomethingHappend += new EventHandler(MyHandler2);
+
+            for (int i=1; i < 30; i++)
+            {
+                notifier.DoSomething(i);
+            }
+        }
+    }
+}
---
+++ CodeS/cSE16_2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeS
+{
+    delegate void MyDelegate(int a);
+
+    class Market
+    {
+        public event MyDelegate CustomerEvent;
+
+        public void BuySomething(int CustomerNo)
+        {
+            if (CustomerNo == 30)
+                CustomerEvent(CustomerNo);
+        }
+    }
+    internal class cSE16_2
+    {
+        static public void MyHandler(int a)
+        {
+            Console.WriteLine("축하합니다! 30번재 고객 이벤트에 당첨되셨습니다");
+        }
+        static void Main(string[] args)
+        {
+            Market market = new Market();
+            market.CustomerEvent += new MyDelegate(MyHandler);
+
+            for ( int customerNo = 0; customerNo < 100; customerNo += 10)
+                market.BuySomething(customerNo);
+
+        }
+    }
+}
